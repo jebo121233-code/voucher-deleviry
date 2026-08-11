921e1b8b46864135ba05af26b96ff53d5a34b4e0
jebo121233-code <jebo121233@gmail.com>
2026-08-11 08:23:48 +0300

    Update data.js

--- frontend/voucher/src/data/data.js
+++ frontend/voucher/src/data/data.js
@@ -513,7 +513,7 @@ percentage: "10%",
 status: "Active",
 addresses: ["شارع المتحف من شارع سعيد "],
 logos: ["wahed_metabaq.jpeg"],
-images: []
+images: ["metapapq.1.jpg","metapaq.2.jpg"]
 },
 
 {
@@ -537,7 +537,7 @@ percentage: "10%",
 status: "Active",
 addresses: ["طنطا-بجوار سور الاستاد"],
 logos: ["palstine.jpeg"],
-images: []
+images: ["palestine1.jpg","palestine.2.jpg"]
 },
 
 {
@@ -561,7 +561,7 @@ percentage: "12%",
 status: "Active",
 addresses: [" شارع سعيد بجوار مدرسة الامريكان"],
 logos: ["frisky.jpeg"],
-images: []
+images: ["frisky.1.jpg","frisky.2.jpg","frisky.3.jpg","frisky.4.jpg","frisky.5.jpg","frisky6.jpg","frisky.7.jpg","frisky.8.jpg","frisky9jpg","frisky.10.jpg","frisky.11.jpg","frisky.12.jpg","frisky.13.jpg","frisky.14.jpg","frisky.15.jpg","frisky.16.jpg","frisky.17.jpg","frisky.18.jpg","frisky.19.jpg","frisky.20.jpg","frisky.21.jpg","frisky.22.jpg"]
 },
 
 {
@@ -573,7 +573,7 @@ percentage: "10%",
 status: "Active",
 addresses: ["طنطا-الاستاد بجوار نادي سيتي كلوب"," الزقازيق-طلبه عويضه مع الخشاب"],
 logos: ["menem.jpeg"],
-images: []
+images: ["menem.1.jpg","menem.2.jpg"]
 },
 
 {
@@ -585,7 +585,7 @@ percentage: "20%",
 status: "Active",
 addresses: ["الاستاد امام كورنيش المرشحة"],
 logos: ["Hanover.jpeg"],
-images: []
+images: ["hanover.1.jpg","hanover.2.jpg","hanover.3.jpg","hanover.4.jpg","hanover.5.jpg","hanover.6.jpg","hanover.7.jpg","hanover.8.jpg","hanover.9.jpg","hanover.10.jpg"]
 },
 
 {
