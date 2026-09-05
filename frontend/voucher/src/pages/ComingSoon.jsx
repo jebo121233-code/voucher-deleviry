5939844101bba354fd08ee69eb282f42c544b238
jebo121233-code <jebo121233@gmail.com>
2026-09-05 14:04:08 +0300

    Create ComingSoon.jsx

---
+++ frontend/voucher/src/pages/ComingSoon.jsx
@@ -0,0 +1,8 @@
+export default function ComingSoon() {
+  return (
+    <div style={{ textAlign: "center", padding: "60px 20px" }}>
+      <h2>🎟️ قريباً</h2>
+      <p>عروض وخصومات استثنائية هتلاقيها هنا قريباً</p>
+    </div>
+  );
+}
